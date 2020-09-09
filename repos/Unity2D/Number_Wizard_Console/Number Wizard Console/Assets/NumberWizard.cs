9907e5090939cb757f23ae5fae86789add72afea
Eric Mock <ericmock@live.com>
2020-09-09 14:06:11 -0700

    Completed Section 2: 'Number Wizard - Basic C# Coding'.

---
+++ repos/Unity2D/Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start() {
+        UnityEngine.Debug.Log("Welcome to number wizard");
+        StartGame();               
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        
+        UnityEngine.Debug.Log("Pick a number");
+        UnityEngine.Debug.Log("Highest number is: " + max);
+        UnityEngine.Debug.Log("Lowest number is: " + min);
+        UnityEngine.Debug.Log("Is your number higher or lower than: " + guess + "?");
+        UnityEngine.Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
+        max++;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            min = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            max = guess;
+            guess = (max + min) / 2;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return)) {
+            UnityEngine.Debug.Log("Great! Let's play again!");
+            StartGame();
+        }
+
+
+
+
+    }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+        UnityEngine.Debug.Log("Is your number higher or lower than: " + guess + "?");
+    }
+}
